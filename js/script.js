0a61b328098f807335bb60f0778bb4c47834bcc0
joyeonjoo <65939709+joyeonjoo@users.noreply.github.com>
2025-08-05 11:04:29 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Swiper 초기화
+// swiper 초기화
 const thumbSwiper = new Swiper('.thumb-swiper', {
   slidesPerView: 3,
   spaceBetween: 10,
@@ -32,4 +32,24 @@ document.querySelectorAll('.slide-img').forEach(img => {
 });
 
 modalClose.onclick = () => modal.style.display = 'none';
+
 modal.onclick = (e) => { if (e.target === modal) modal.style.display = 'none'; }
+
+// 사이드바
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
+let searchBtn = document.querySelector(".bx-search");
+let listItem = document.querySelectorAll(".list-item");
+btn.onclick = function(){
+  sidebar.classList.toggle("active");
+}
+searchBtn.onclick = function(){
+  sidebar.classList.toggle("active");
+}
+function activeLink(){
+  listItem.forEach(item =>
+  item.classList.remove("active"));
+  this.classList.add("active");
+}
+listItem.forEach(item => 
+item.onclick = activeLink);
