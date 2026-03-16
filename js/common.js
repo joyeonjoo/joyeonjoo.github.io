ad5316404d0f09e0ce380345ac68a8ccce647c63
joyeonjoo <65939709+joyeonjoo@users.noreply.github.com>
2026-03-16 10:14:00 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,4 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
+   gsap.registerPlugin(ScrollTrigger);
+    // 2. 프로젝트 카드 스크롤 애니메이션
+    gsap.utils.toArray('.portfolio-project').forEach(card => {
+      gsap.from(card, {
+        scrollTrigger: {
+          trigger: card,
+          start: "top 100%",
+        },
+        y: 50, opacity: 0, duration: 1, ease: "power2.out"
+      });
+    });
+
+    // 3. 스킬 원형 인디케이터 (스크롤 시 작동)
+    gsap.utils.toArray('.progress').forEach(circle => {
+      const target = circle.getAttribute('data-target');
+      const circumference = 2 * Math.PI * 45;
+
+      gsap.to(circle, {
+        scrollTrigger: {
+          trigger: circle,
+          start: "bottom 10%",
+        },
+        strokeDashoffset: circumference - (target / 100 * circumference),
+        duration: 2,
+        ease: "power2.inOut"
+      });
+    });
   const modal = document.getElementById('main-modal');
   const closeBtn = document.getElementById('close-btn');
   
